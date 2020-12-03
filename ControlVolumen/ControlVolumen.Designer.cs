8aa60174c12ee98073e2868223d17ac58095aa71
aarenzanac <aarenzana@gmail.com>
2020-12-03 16:45:49 +0100

    Inicial

--- ControlVolumen/ControlVolumen.Designer.cs
+++ ControlVolumen/ControlVolumen.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace ControlVolumen
 {
-    partial class UserControl1
+    partial class ControlVolumen
     {
-        /// <summary>
+        /// <summary> 
         /// Variable del diseñador necesaria.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Limpiar los recursos que se estén usando.
         /// </summary>
         /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
@@ -22,7 +22,7 @@
 
         #region Código generado por el Diseñador de componentes
 
-        /// <summary>
+        /// <summary> 
         /// Método necesario para admitir el Diseñador. No se puede modificar
         /// el contenido de este método con el editor de código.
         /// </summary>
@@ -30,12 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // UserControl1
+            // ControlVolumen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "UserControl1";
-            this.Size = new System.Drawing.Size(531, 275);
+            this.Name = "ControlVolumen";
+            this.Size = new System.Drawing.Size(448, 263);
+            this.Layout += new System.Windows.Forms.LayoutEventHandler(this.ControlVolumen_Layout);
             this.ResumeLayout(false);
 
         }
---
+++ ControlVolumen/ControlVolumen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlVolumen
+{
+    public partial class ControlVolumen : UserControl
+    {
+        public enum NivelVolumen { 
+            alto,
+            medio,
+            bajo
+        }
+
+        private NivelVolumen estadoVolumen;
+        public ControlVolumen()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            Point punto1 = new Point(0, 0);
+            Point punto2 = new Point(0, this.Height);
+            Point punto3 = new Point(this.Width, this.Height);
+            Point[] triangulo = { punto2, punto1, punto3 };
+            g.FillPolygon(Brushes.Gray, triangulo);
+                
+        }
+
+        private void ControlVolumen_Layout(object sender, LayoutEventArgs e)
+        {
+            if (e.AffectedProperty == "Bounds") {
+                this.Width = 2 * this.Height;
+            }
+        }
+    }
+}
